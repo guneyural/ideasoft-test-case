6a470a4dcf4816aa6a49721c360e10c33478212c
guneyural <guneyural@gmail.com>
2024-12-10 12:57:02 +0300

    add types for product item

--- src/store/slices/Product/types.ts
+++ src/store/slices/Product/types.ts
@@ -1,5 +1,21 @@
 export interface ProductState {
   isLoading: boolean;
   error: { message: null | string; status: null | number } | null;
-  products: any[];
+  products: ProductType[];
+}
+
+export interface ProductType {
+  id: number;
+  images: string[];
+  name: string;
+  stockAmount: number;
+  createdAt: Date;
+  taxIncluded: number;
+  price1: number;
+  currency: { id: number; abbr: string; label: string };
+  tax: number;
+  hasGift: number;
+  discount: number;
+  discountType: number;
+  stockTypeLabel: string;
 }
